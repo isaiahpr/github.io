141f06f0a48185769eb9759fd95dcff4ef4fdde4
isaiahpr <ip.ricardos@gmail.com>
2024-08-22 21:30:47 -0400

    adding back headshot

--- javascript/index.js
+++ javascript/index.js
@@ -9,6 +9,7 @@ var values = [{
     subject: 'english',
     corps_region: 'Hawaii',
     corps_year: 2022,
+    headshot: '<img alt="Headshot of Dayla" data-entity-type="" data-entity-uuid="" src="https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_square.jpg" />',
   },
   {
     name: 'Jonas Arnklint',
